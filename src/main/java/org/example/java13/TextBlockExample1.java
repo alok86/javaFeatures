e3ce5cf07462c38ac81429850dcb083b5dfad128
alok seth <alok.seth4@gmail.com>
2023-12-23 11:42:34 +0530

    java 13 Text Block

---
+++ src/main/java/org/example/java13/TextBlockExample1.java
@@ -0,0 +1,12 @@
+package org.example.java13;
+
+public class TextBlockExample1 {
+    public static void main(String[] args) {
+        String inputElement= """
+                Name : Jenny
+                Phone : 8675309
+                Age : 35
+                """;
+        System.out.println(inputElement);
+    }
+}
