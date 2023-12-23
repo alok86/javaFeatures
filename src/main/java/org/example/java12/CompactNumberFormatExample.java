8cec2e36c702a8d2e746ba704b7a241238aad2d5
alok seth <alok.seth4@gmail.com>
2023-12-23 13:11:09 +0530

    java 12 CompactNumberFormatExample1

--- src/main/java/org/example/java12/CompactNumberFormatExample.java
+++ src/main/java/org/example/java12/CompactNumberFormatExample.java
@@ -1,6 +1,8 @@
 package org.example.java12;
 
+import java.math.RoundingMode;
 import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.List;
 import java.util.Locale;
 
@@ -11,16 +13,25 @@ public class CompactNumberFormatExample {
     }
 
     private static void formatForLocal(Locale locale) {
-        List<Integer> number = List.of(1000,1000000,1000000000);
+        List<Integer> number = List.of(1500,1500000,1200000000);
         System.out.printf("--SHORT format for local = %s -- %n" , locale);
         number.stream().forEach((num)->{
             NumberFormat nf = NumberFormat.getCompactNumberInstance(locale, NumberFormat.Style.SHORT);
+            try {
+                Number n = nf.parse("3M");
+                System.out.println(n);
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+
+            nf.setRoundingMode(RoundingMode.HALF_DOWN);
             String format = nf.format(num);
             System.out.println(format);
         });
         System.out.printf("-- LONG format for locale=%s --%n", locale);
         number.forEach((num) -> {
             NumberFormat nf = NumberFormat.getCompactNumberInstance(locale, NumberFormat.Style.LONG);
+            nf.setRoundingMode(RoundingMode.HALF_DOWN);
             String format = nf.format(num);
             System.out.println(format);
         });
